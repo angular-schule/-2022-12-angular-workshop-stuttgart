15b1334ddf65efc89efcae84c5e09ed57d09d81c
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-12-08 15:50:46 +0100

    Aquila Components: User Interface (UI) library by Allianz
    https://allianz.github.io/ng-aquila/welcome

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -5,6 +5,12 @@ import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 
+import { NxGridModule } from '@aposin/ng-aquila/grid';
+import { NxCardModule } from '@aposin/ng-aquila/card';
+import { NxButtonModule } from '@aposin/ng-aquila/button';
+import { NxHeadlineModule } from '@aposin/ng-aquila/headline';
+import { NxCopytextModule } from '@aposin/ng-aquila/copytext';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +19,12 @@ import { BookComponent } from './book/book.component';
   ],
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    NxCardModule,
+    NxButtonModule,
+    NxGridModule,
+    NxHeadlineModule,
+    NxCopytextModule
   ],
   exports: [
     DashboardComponent
