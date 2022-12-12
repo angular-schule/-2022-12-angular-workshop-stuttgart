f85569925959afc7339c8cc68c6c9dcc64a9ab5d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-12-12 10:48:17 +0100

    testing behaviour with spies

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -2,40 +2,32 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookComponent } from '../book/book.component';
 import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 import { DashboardComponent } from './dashboard.component';
 
 
-@Component({
-  selector: 'br-book',
-  template: 'Test'
-})
-export class DummyBookComponent {
-
-  @Input()
-  book?: Book;
-
-  @Output()
-  rateUp = new EventEmitter<Book>();
-
-  @Output()
-  rateDown = new EventEmitter<Book>();
-}
-
-
-describe('DashboardComponent', () => {
+fdescribe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
+
+    const bookRatingMock = {
+      rateUp: (book: Book) => book
+    }
+
     await TestBed.configureTestingModule({
       declarations: [
         DashboardComponent,
         // Integration Test
-        // BookComponent
-
-        // Unit test
-        DummyBookComponent
+        BookComponent
+      ],
+      providers: [
+        {
+          provide: BookRatingService,
+          useValue: bookRatingMock
+        }
       ]
     })
     .compileComponents();
@@ -45,7 +37,14 @@ describe('DashboardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('doRateUp() should forward all calls to BookRatingService', () => {
+
+    const rs = TestBed.inject(BookRatingService);
+    spyOn(rs, 'rateUp').and.callThrough();
+
+    const book = { } as Book;
+    component.doRateUp(book);
+
+    expect(rs.rateUp).toHaveBeenCalledOnceWith(book);
   });
 });
