8fff74ad1a4fcae0c2a4c741b6b60a6afd6783d2
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-12-13 22:11:17 +0100

    cypress: both e2e and component tests are working 🎉, no type errors for jasmine specs

---
+++ book-rating/src/app/books/book/book.component.cy.ts
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+// /// <reference path="/cypress/support/component.ts" />
+
+import { BookComponent } from "./book.component";
+
+
+it('mounts', () => {
+  cy.mount(BookComponent, {
+    componentProperties: {
+      book: {
+        isbn: '000',
+        title: 'Test Title',
+        description: 'Component Test',
+        rating: 5,
+        price: 1
+      },
+    },
+  });
+
+  cy.get('h2').contains('Test Title');
+})
