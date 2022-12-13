7840e5401b214bd046eeca545accfd3609a37e6c
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-12-13 10:44:40 +0100

    forms: adding aquila form controls (+grid)

---
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'br-book-create',
+  templateUrl: './book-create.component.html',
+  styleUrls: ['./book-create.component.scss']
+})
+export class BookCreateComponent {
+
+  bookForm = new FormGroup({
+
+    isbn: new FormControl('' , {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(3)
+      ]
+    }),
+
+    title: new FormControl('' , {
+      nonNullable: true,
+      validators: [
+        Validators.required
+      ]
+    }),
+
+    rating: new FormControl(1 , {
+      nonNullable: true,
+      validators: [
+        Validators.min(1),
+        Validators.max(5)
+      ]
+    }),
+
+    description: new FormControl('' , {
+      nonNullable: true
+    })
+  })
+}
--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NxButtonModule } from '@aposin/ng-aquila/button';
 import { NxCardModule } from '@aposin/ng-aquila/card';
 import { NxCopytextModule } from '@aposin/ng-aquila/copytext';
@@ -7,8 +8,10 @@ import { NxGridModule } from '@aposin/ng-aquila/grid';
 import { NxHeadlineModule } from '@aposin/ng-aquila/headline';
 import { NxIconModule } from '@aposin/ng-aquila/icon';
 import { NxInputModule } from '@aposin/ng-aquila/input';
+import { NxNumberStepperModule } from '@aposin/ng-aquila/number-stepper';
 import { NxRatingModule } from '@aposin/ng-aquila/rating';
 
+import { BookCreateComponent } from './book-create/book-create.component';
 import { BookComponent } from './book/book.component';
 import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -17,11 +20,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
     DashboardComponent,
-    BookComponent
+    BookComponent,
+    BookCreateComponent
   ],
   imports: [
     CommonModule,
     BooksRoutingModule,
+    ReactiveFormsModule,
 
     NxButtonModule,
     NxCardModule,
@@ -30,7 +35,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     NxHeadlineModule,
     NxIconModule,
     NxInputModule,
-    NxRatingModule
+    NxNumberStepperModule,
+    NxRatingModule,
   ],
   exports: [
     DashboardComponent
